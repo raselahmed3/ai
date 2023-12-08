3e104e33a66addf0b57bbffb3633b887a566d089
Rasel Ahmed <raselbabu34.bd@gmail.com>
2023-12-08 16:49:16 +0600

    init

---
+++ app/Http/Controllers/AiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class AiController extends Controller
+{
+    public function index()
+    {
+       return view('welcome');
+    }
+}
